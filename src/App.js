1ef4566ecbb6a1210c1a07adb4b613c891806345
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-03-09 23:39:23 -0700

    Animation and Pseudo Selectors | #2.4

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Btn = styled.button`
-  background-color: violet;
-  color: white;
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0;
+  }
 `;
-
-const Input = styled.input.attrs({ required: true, maxLength: 30 });
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: orange;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotateAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 48px;
+    }
+  }
+`;
 
 function App() {
   return (
-    <Father>
-      <Btn>Login</Btn>
-      <Btn as="a" href="">
-        Join
-      </Btn>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>🙂</span>
+      </Box>
+    </Wrapper>
   );
 }
 
